c701dc6e636f9b905637392df0377dad914f7e51
Aamir <amir@shopstan.pk>
2021-02-01 21:17:46 +0500

    auto dep build and dep using cd pipeline

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world updated today\n');
+  res.send('Hello world updated today | cd updated\n');
 });
 
 app.listen(PORT, HOST);
